3c6076780f76c0d58c62c014884f58311d8392ef
Sayan Ghosh <sg4812036@gmail.com>
2026-05-06 22:47:15 +0530

    Add vector practice code

---
+++ Vector.cpp/hello.cpp
@@ -0,0 +1,57 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> vec = {1, 2, 3, 4, 5};
+//     cout << vec[2] << endl;
+//     return 0;
+// }
+// for each loop with vector of characters
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<char> vec {'a', 'b', 'c', 'd', 'e'};
+
+//     for(char i : vec) {
+//         cout << i << endl;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<char> vec {'a', 'b', 'c', 'd', 'e'};
+//     vec.push_back('f');
+//     cout << "after push back size = " << vec.size() << endl;
+
+//     vec.pop_back();
+//     cout << "after pop back size = " << vec.size() << endl;
+
+//     for(char i : vec) {
+//         cout << i << endl;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> vec;
+
+//     vec.push_back(1);
+//     vec.push_back(2);
+//     vec.push_back(3);
+
+//     cout << "size = " << vec.size() << endl;
+//     cout << "capacity = " << vec.capacity() << endl;
+
+//     return 0;
+// }
